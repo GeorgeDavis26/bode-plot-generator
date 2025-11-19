b25e70de8448a9975e44ac1220b70df2f37a4b52
George Davis <gdavis@g.hmc.edu>
2025-11-18 21:14:26 -0800

    init MCU branch

---
+++ mcu/lib/main.h
@@ -0,0 +1,15 @@
+/**
+    Main Header: Contains general defines and selected portions of CMSIS files
+    @file main.h
+    @author George DAvis
+    @version 10/27/2025
+
+    Main header file for Bode Plot Generator IOT function.
+*/
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "STM32L432KC.h"
+
+#endif // MAIN_H
